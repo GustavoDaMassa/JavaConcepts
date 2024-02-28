562bd1e0fed81c9a92920676cc49ac8ee62e3deb
Gustavo Henrique <gustavo.pereira@discente.ufg.br>
2024-02-28 10:17:50 -0300

    Throwable/ - Renomeando

---
+++ Throwable/ThrowableCODE/Exceptions/RunTimeExceptions.java
@@ -0,0 +1,12 @@
+package Exceptions.ThrowableCODE.Exceptions;
+
+public class RunTimeExceptions {
+    public static void main(String[] args) {
+
+        Object object = null;
+        System.out.println(object.toString()); // teremos uma NullPointerException
+
+        int[] numeros = { 1, 2 };
+        System.out.println(numeros[9]); // ArrayIndexOutOfBoundsException
+    }
+}
