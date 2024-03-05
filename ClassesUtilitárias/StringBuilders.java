868aecc63fb177477db8a86859bb487d1690bfd7
Gustavo Henrique <gustavo.pereira@discente.ufg.br>
2024-03-04 23:35:03 -0300

    Explicando StringBuilder

---
+++ ClassesUtilitárias/StringBuilders.java
@@ -0,0 +1,5 @@
+public class StringBuilders {
+    public static void main(String[] args) {
+        String nome
+    }
+}
---
+++ ClassesUtilitárias/Strings.java
@@ -0,0 +1,18 @@
+import java.lang.String;
+
+public class Strings {
+    public static void main(String[] args) {
+        // string constant pool
+        String nome = "gustavo";
+        String nome2 = "gustavo";
+        nome.concat("henrique");// existe o nome gustavo henrique no pool de strings porém a referencia de nome
+                                // ainda aponta para gustavo
+
+        System.out.println(nome == nome2);
+
+        String nome3 = new String("gustavo");// nesse caso nome3 faz referenia para um objeto e ele sim faz referencia
+                                             // pra o gustavo já existente no pool de strings
+        System.out.println(nome == nome3); // false
+        System.out.println(nome == nome3.intern()); // true
+    }
+}
